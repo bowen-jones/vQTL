c0bac5fc4ec2332574df922882342394e0508cdb
Bowen Jones <jonesb93@gmail.com>
2020-08-26 20:50:17 -0400

    change partition for 1k

--- analysis/Stamp.sh
+++ analysis/Stamp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -J rcsim1k      # Job name
 #SBATCH -e rcsimu.%j       # Name of stderr error file
-#SBATCH -p normal          # Queue (partition) name
+#SBATCH -p long          # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
-#SBATCH -t 48:00:00        # Run time (hh:mm:ss)
+#SBATCH -t 90:00:00        # Run time (hh:mm:ss)
 #SBATCH --mail-user=bwj9135@uncw.edu
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
