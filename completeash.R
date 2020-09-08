1fbd63a5a43312b83ee1929137ddf2b0545b5fc0
Bowen Jones <jonesb93@gmail.com>
2020-09-07 23:58:37 -0400

    change mod

--- completeash.R
+++ completeash.R
@@ -8,10 +8,10 @@ setwd("/work/07644/tg869432/stampede2/vQTL/")
 df.breed <- read.csv(file = "simdata3.csv")
 df.breed <- df.breed[-c(1,2),]
 
-mod = dglm(stress ~ ., dformula = ~., data = df.breed)
+mod = dglm(stress ~ ., dformula = ~., data = df.breed, subset = c(1:10))
 
-mod.test<-summary(mod[[1]])$coefficients;
-t1<-ash(mod.test[,1],mod.test[,2])$result;
+mod.test<-mod[1]
+t1<-ash(mod.test[1],mod.test[2])$result;
 results <- cbind(t1[,1:4],mod.test[,4]);
 
 write.csv(mod.test, "dglmcoeffs.csv")
